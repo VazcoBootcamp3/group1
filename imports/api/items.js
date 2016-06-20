71413af1e7072ec640c4a662491fe64677618718
Michal Szewczuk <michal@szewcz.uk>
2016-06-20 10:34:29 +0200

    items.delete

--- imports/api/items.js
+++ imports/api/items.js
@@ -1,3 +1,23 @@
+import {Meteor} from 'meteor/meteor';
 import {Mongo} from 'meteor/mongo';
+import {check} from 'meteor/check';
+
+import {Debts}      from '/imports/api/debts.js';
+
 
 export const Items = new Mongo.Collection('items');
+
+Meteor.methods({
+
+	'items.delete'(itemId) {
+		check(itemId, String);
+
+		if(!this.userId) {
+			throw new Meteor.Error('non-authorized');
+		}
+
+		Debts.remove({item: itemId});
+		Items.remove(itemId);
+	},
+
+});
