5eb4388dfb6183965d23e13ce7753bc69376c1fa
Michal Szewczuk <michal@szewcz.uk>
2016-06-20 10:37:19 +0200

    debts.checkMyDebt, debts.numberOfDebtors, debts.settle

--- imports/api/debts.js
+++ imports/api/debts.js
@@ -1,3 +1,45 @@
+import {Meteor} from 'meteor/meteor';
 import {Mongo} from 'meteor/mongo';
+import {check} from 'meteor/check';
 
 export const Debts = new Mongo.Collection('debts');
+
+Meteor.methods({
+	'debts.checkMyDebt'(itemId) {
+		check(itemId, String);
+
+		if(!this.userId) {
+			throw new Meteor.Error('non-authorized');
+		}
+
+		return Debts.findOne(
+			{
+				debtor: Meteor.userId(),
+				item: itemId,
+			});
+	},
+
+	'debts.numberOfDebtors'(itemId) {
+		check(itemId, String);
+
+		if(!this.userId) {
+			throw new Meteor.Error('non-authorized');
+		}
+
+		return Debts.find({item: itemId,}).count();
+	},
+
+	'debts.settle'(itemId) {
+		check(itemId, String);
+
+		if(!this.userId) {
+			throw new Meteor.Error('non-authorized');
+		}
+
+		Debts.update(
+			{item: itemId, debtor: this.userId},
+			{$set: {cost: 0}},
+		);
+	}
+});
+
