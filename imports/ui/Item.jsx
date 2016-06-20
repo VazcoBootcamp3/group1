d683b46511eb44444cc81776271077ac83e974ad
Michal Szewczuk <michal@szewcz.uk>
2016-06-20 10:38:00 +0200

    item-shared

--- imports/ui/Item.jsx
+++ imports/ui/Item.jsx
@@ -1,5 +1,6 @@
 import React, {Component, PropTypes} from 'react';
 import {Meteor} from 'meteor/meteor';
+import {createContainer}  from 'meteor/react-meteor-data';
 
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
@@ -12,35 +13,110 @@ import {List, ListItem} from 'material-ui/List';
 import {red100, green100} from 'material-ui/styles/colors';
 
 import {Items} from '/imports/api/items.js';
+import {Debts} from '/imports/api/debts.js';
 
-export default class Item extends Component {
-	_deleteItem() {
-		Items.remove(this.props.item._id);
-	}
+import randomColor from 'random-color';
+
+import {
+blue300,
+indigo900,
+orange200,
+deepOrange300,
+pink400,
+purple500,
+} from 'material-ui/styles/colors';
+
+export class Item extends Component {
 
 
 	_btnSettle() {
-		return(
-		<RaisedButton
-	      label="SETTLE"
-	      backgroundColor={green100}
-	      icon={<FontIcon className="material-icons">account_balance_wallet</FontIcon>}
-	      className="item-btn"
-	    />	
-	    );
+		if(Meteor.userId() !== this.props.item.creditor && this._checkMyDebt() > 0) {
+			return(
+				<RaisedButton
+			      label="SETTLE"
+			      backgroundColor={green100}
+			      icon={<FontIcon className="material-icons">account_balance_wallet</FontIcon>}
+			      className="item-btn"
+			      onTouchTap={this._settle.bind(this)}
+			    />	
+	    	);
+		}
 	}
 
 	_btnDelete() {
-		if(Meteor.userId() === this.props.item.creditor) 
+		if(Meteor.userId() === this.props.item.creditor) {
 			return(
 		       	<RaisedButton
 			      label="DELETE"
 			      backgroundColor={red100}
 			      icon={<FontIcon className="material-icons">delete</FontIcon>}
 			      className="item-btn"
-			      onTouchTap={this._deleteItem.bind(this)}
+			      onTouchTap={this._delete.bind(this)}
 			    />		
 		    );
+		}
+	}
+
+	_settle() {
+		Meteor.call('debts.settle', this.props.item._id);
+	}
+
+	_delete() {
+		Meteor.call('items.delete', this.props.item._id);
+	}
+
+	// to remove
+	//======================================================
+	getUsername(id) {
+		return Meteor.users.findOne({_id: id}).username;
+	}
+
+	_checkMyDebt() {
+		let debt = Debts.findOne(
+			{
+				item: this.props.item._id,
+				debtor: Meteor.userId(),
+			});
+		return debt ? debt.cost : 0;
+	}
+
+	_renderShareWith() {
+		return this.props.debts.map((value, key) => {
+			return(
+				<Avatar
+					key={key} 
+					className="item-avatar" 
+					size={40}
+	          		color={indigo900}
+	          		backgroundColor={blue300}>
+				{this.getUsername(value.debtor)[0]}
+				</Avatar>
+			);
+		});
+	}
+
+	_renderStillGuilty() {
+		return this.props.debts.map((value, key) => {
+			if(value.cost > 0)
+				return(
+					<Avatar
+						key={key} 
+						className="item-avatar" 
+						size={40}
+		          		color={indigo900}
+		          		backgroundColor={blue300}>
+					{this.getUsername(value.debtor)[0]}
+					</Avatar>
+				);
+		});	
+	}
+
+	_renderShouldPay() {
+		if(Meteor.userId() === this.props.item.creditor)
+			return 'nothing.';
+		else {
+			return this._checkMyDebt().toFixed(2) + ' zł';
+		}
 	}
 
 	render() {
@@ -71,11 +147,11 @@ export default class Item extends Component {
 			    	<div className="column-right">
 			    		<div className="item-box">
 			    			<Subheader>You should pay:</Subheader>
-					    	<p className="item-t item-cost item-your-cost">0.50 zł</p>
+					    	<p className="item-t item-cost item-your-cost">{this._renderShouldPay()}</p>
 				    	</div>
 
 			    		<div className="item-box">
-			    			<Subheader>Options:</Subheader>
+			    			<Subheader>Avaible options:</Subheader>
 			          		{this._btnSettle()}
 			          		{this._btnDelete()}
 			          	</div>
@@ -85,17 +161,14 @@ export default class Item extends Component {
 			    		<div className="item-box">
 			    			<Subheader>Shared with:</Subheader>
 				    		<div className="item-t">
-					    		<Avatar className="item-avatar" src="http://material-ui.com/images/uxceo-128.jpg" size={40} />
-					    		<Avatar className="item-avatar" src="http://material-ui.com/images/uxceo-128.jpg" size={40} />
-					    		<Avatar className="item-avatar" src="http://material-ui.com/images/uxceo-128.jpg" size={40} />
-					    		<Avatar className="item-avatar" src="http://material-ui.com/images/uxceo-128.jpg" size={40} />
+								{this._renderShareWith()}
 					    	</div>
 				    	</div>
 
 			    		<div className="item-box">
 					    	<Subheader>Still guilty:</Subheader>
 				    		<div className="item-t">
-					    		<Avatar className="item-avatar" src="http://material-ui.com/images/uxceo-128.jpg" size={40} />
+				    			{this._renderStillGuilty()}
 					    	</div>
 				    	</div>
 				    </div>
@@ -104,3 +177,9 @@ export default class Item extends Component {
 		);
 	}
 }
+
+export default createContainer((props) => {
+  return{
+  	debts: Debts.find({item: props.item._id}).fetch(),
+  };
+}, Item);
