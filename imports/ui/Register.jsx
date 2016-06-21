8fed46ec51471a720f2502e3f45c45da68818dcd
Michal Szewczuk <michal@szewcz.uk>
2016-06-21 11:23:05 +0200

    add _handleStep1

--- imports/ui/Register.jsx
+++ imports/ui/Register.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Random} from 'meteor/random';
 
 
 import TextField from 'material-ui/TextField';
@@ -8,6 +9,10 @@ import Subheader from 'material-ui/Subheader';
 import {Step,Stepper,StepLabel,StepContent} from 'material-ui/Stepper';
 import FlatButton from 'material-ui/FlatButton';
 import Toggle from 'material-ui/Toggle';
+import {List, ListItem} from 'material-ui/List';
+import Checkbox from 'material-ui/Checkbox';
+
+import '/imports/api/users.js';
 
 
 export default class extends React.Component {
@@ -17,7 +22,10 @@ export default class extends React.Component {
             finished: false,
             stepIndex: 0,
             showPassword: false,
+            account: {},
         }
+
+        this._renderStepActions = this._renderStepActions.bind(this);
     }
 
     _nextStep() {
@@ -34,38 +42,117 @@ export default class extends React.Component {
             });
     }
 
+    _gotoStep(step) {
+        this.setState({
+            stepIndex: step,
+            finished: false,
+        });
+    }
+
     _renderStepActions(step) {
         const {stepIndex} = this.state;
 
         return (
-          <div className="login-btn-bar">
-            <RaisedButton
-              label={stepIndex === 2 ? 'Finish' : 'Next'}
-              disableTouchRipple={true}
-              disableFocusRipple={true}
-              primary={true}
-              onTouchTap={this._nextStep.bind(this)}
-              className="login-btn"
-            />
+            <div className="login-btn-bar">
+                <RaisedButton
+                    label='Next'
+                    disableTouchRipple={true}
+                    disableFocusRipple={true}
+                    primary={true}
+                    onTouchTap={this._nextStep.bind(this)}
+                    className="login-btn"
+                />
 
             {step > 0 && (
-              <FlatButton
-                label="Back"
-                disabled={stepIndex === 0}
-                disableTouchRipple={true}
-                disableFocusRipple={true}
-                onTouchTap={this._prevStep.bind(this)}
-              />
+                <FlatButton
+                    label="Back"
+                    disabled={stepIndex === 0}
+                    disableTouchRipple={true}
+                    disableFocusRipple={true}
+                    onTouchTap={this._prevStep.bind(this)}
+                />
             )}
-
-          </div>
+            </div>
         );
     }
+
 
     _showPassword() {
         this.setState({
             showPassword: !this.state.showPassword,  
         });
+    }
+
+    _handleStep1() {
+        // ?
+        //const {username, password, email, phone} = this.refs;
+        const username = this.refs.username.getValue();
+        const password = this.refs.password.getValue();
+        const email = this.refs.email.getValue();
+        const phone = this.refs.phone.getValue();
+
+        if(!username || !password || !email || !phone) {
+            alert('You must fill the required fields.');
+            return;
+        }
+
+        Meteor.call('user.exists', username, (error, result) => {
+            if(error) {
+                console.log(error);
+                return;
+            }
+
+            if(result === true) {
+                alert('Username ' + username + ' already exists.');
+            }
+            else {
+                this.setState({
+                    account: {
+                                username: username,
+                                password: password,
+                                email: email,
+                                phone: phone,
+                             },  
+                });
+                this._nextStep();
+            }
+        });
+        
+
+    }
+
+    _handleRegister(e) {
+        e.preventDefault();
+/*
+        const username = this.refs.username.getValue();
+        const password = this.refs.password.getValue();
+        const email = this.refs.email.getValue();
+        const phone = this.refs.phone.getValue();
+
+        if(!username || !password || !email || !phone) {
+            alert("ERROR");
+            return;
+        }
+*/
+        const groupName = this.refs.group;
+        console.log(groupName);
+
+        if(!groupName) {
+            alert("GROUP ERROR");
+            return;
+        }
+
+        let userId = Random.id();
+
+        console.log('meteor.call ...' + userId);
+
+        Meteor.call('groups.create', groupName, userId, (error, result) => {
+            if(error) console.log(error);
+            if(result) console.log(result);
+        });
+
+
+
     }
 
     render() {
@@ -78,11 +165,11 @@ export default class extends React.Component {
                             <StepLabel>Fill the register form</StepLabel>
                             <StepContent>
                                 <div className="login-fields">
-
                                     <TextField
                                         floatingLabelText="Username"
                                         fullWidth={true}
                                         ref="username"
+                                        defaultValue={this.state.account.username}
                                     />
 
                                     <TextField
@@ -93,28 +180,70 @@ export default class extends React.Component {
                                     />
 
                                     <Toggle
-                                      label="Show my password"
-                                      labelPosition="right"
-                                      onToggle={this._showPassword.bind(this)}
+                                        label="Show my password"
+                                        labelPosition="right"
+                                        onToggle={this._showPassword.bind(this)}
                                     />
 
                                     <TextField
                                         floatingLabelText="Email"
                                         fullWidth={true}
+                                        ref="email"
+                                        defaultValue={this.state.account.email}
                                     />
 
                                     <TextField
                                         floatingLabelText="Phone number"
                                         fullWidth={true}
+                                        ref="phone"
+                                        defaultValue={this.state.account.phone}
                                     />
                                 </div>
-                                {this._renderStepActions(0)}
+
+                                <div className="login-btn-bar">
+                                    <RaisedButton
+                                        label='Next'
+                                        disableTouchRipple={true}
+                                        disableFocusRipple={true}
+                                        primary={true}
+                                        onTouchTap={this._handleStep1.bind(this)}
+                                        className="login-btn"
+                                    />
+                                </div> 
+                                
                             </StepContent>
                         </Step>
 
                         <Step>
                             <StepLabel>Find group or create your own</StepLabel>
                             <StepContent>
+                                <Subheader>Choose one from below:</Subheader>
+                                <List>
+                                    <ListItem
+                                      leftCheckbox={<Checkbox />}
+                                      primaryText="Notifications"
+                                      secondaryText="Allow notifications"
+                                    />
+                                    <ListItem
+                                      leftCheckbox={<Checkbox />}
+                                      primaryText="Sounds"
+                                      secondaryText="Hangouts message"
+                                    />
+                                    <ListItem
+                                      leftCheckbox={<Checkbox />}
+                                      primaryText="Video sounds"
+                                      secondaryText="Hangouts video call"
+                                    />
+                                </List>
+
+                                <Subheader>or create your own:</Subheader>
+                                <TextField
+                                    className="register-group-field"
+                                    floatingLabelText="Group name"
+                                    fullWidth={true}
+                                    ref="group"
+                                />
+           
                             {this._renderStepActions(1)}
                             </StepContent>
                         </Step>
@@ -123,7 +252,17 @@ export default class extends React.Component {
                             <StepLabel>Have fun</StepLabel>
                             <StepContent>
                             <p>If you'll have any troubles, please contact with me at abc@dot.dot, good luck ;)</p>
-                            {this._renderStepActions(2)}
+
+                            <div className="login-btn-bar">
+                                <RaisedButton
+                                    label='Finish'
+                                    disableTouchRipple={true}
+                                    disableFocusRipple={true}
+                                    primary={true}
+                                    onTouchTap={this._handleRegister.bind(this)}
+                                    className="login-btn"
+                                />
+                            </div>   
                             </StepContent>
                         </Step>
                     </Stepper>
