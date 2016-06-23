32e69c4ab31f59515338f0f961e2a51ad6acee4a
Mateusz Stanuch <mateusz.stanuch@gmail.com>
2016-06-23 17:26:49 +0200

    Update: Login Component is now Stateless

--- mateuszstanuch/components/Login.jsx
+++ mateuszstanuch/components/Login.jsx
@@ -1,52 +1,43 @@
 import React from 'react/react';
 
-export default class Login extends React.Component {
-    constructor (...args) {
-        super(...args);
-        this.onSubmit = this.onSubmit.bind(this);
-    }
+function onSubmit (e) {
+    e.preventDefault();
 
-    onSubmit (e) {
-        e.preventDefault();
+    const form = e.target;
 
-        let username = this.refs.username.value;
-        let password = this.refs.password.value;
+    let username = form.querySelector('[name=username]').value;
+    let password = form.querySelector('[name=password]').value;
 
-        if(password !== '' && username != '') {
-            
-            Meteor.loginWithPassword(username, password, (error) => {
-                if(error) {
-                    Materialize.toast(error.reason, 4000);
-                    return;
-                }
+    if(password !== '' && username != '') {
+        Meteor.loginWithPassword(username, password, (error) => {
+            if(error) {
+                Materialize.toast(error.reason, 4000);
+                return;
+            }
 
-                FlowRouter.go('Home');
-            });
-
-        } else {
-            Materialize.toast("Wypełnij wszystkie pola", 4000);
-        }
-    }
-
-    componentWillMount () {
-        if( this.props.isLoggedIn )
+            Materialize.toast("Pomyślnie zalogowano!", 4000);
             FlowRouter.go('Home');
-    }
-
-    render () {
-        return (
-            <div className="row">
-                <h4>Logowanie</h4>
-                <form className="col offset-s3 s6" onSubmit={this.onSubmit}>
-                    <div className="input-field col s12">
-                        <input type="text" placeholder="Nazwa użytkownika" ref="username" />
-                    </div>
-                    <div className="input-field col s12">
-                        <input type="password" placeholder="Hasło" ref="password" />
-                    </div>
-                    <button className="waves-effect waves-light btn-large">Zaloguj</button>
-                </form>
-            </div>
-        );
+        });
+    } else {
+        Materialize.toast("Wypełnij wszystkie pola", 4000);
     }
 }
+
+const Login = (props) => {
+    return (
+        <div className="row">
+            <h4>Logowanie</h4>
+            <form className="col offset-s3 s6" onSubmit={onSubmit}>
+                <div className="input-field col s12">
+                    <input type="text" placeholder="Nazwa użytkownika" name="username" />
+                </div>
+                <div className="input-field col s12">
+                    <input type="password" placeholder="Hasło" name="password" />
+                </div>
+                <button className="waves-effect waves-light btn-large">Zaloguj</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
