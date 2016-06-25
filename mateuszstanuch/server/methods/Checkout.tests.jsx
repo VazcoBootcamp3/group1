e154ada705a49366b104beaf573a17d8115fb9fe
Mateusz Stanuch <mateusz.stanuch@gmail.com>
2016-06-25 18:31:24 +0200

    Test: add basics Checkout tests

--- mateuszstanuch/server/methods/Checkout.tests.jsx
+++ mateuszstanuch/server/methods/Checkout.tests.jsx
@@ -11,13 +11,38 @@ if( Meteor.isServer ) {
         describe('methods', () => {
             const user1_name = 'user1';
             const user2_name = 'user2';
+            const user3_name = 'user3';
+            const groupName = 'test';
 
             beforeEach(() => {
                 Meteor.users.remove({});
-                Accounts.createUser({username: user1_name});
-                Accounts.createUser({username: user2_name});
-
+                // clear GroupList
+                GroupList.remove({});
+                // clear ShoppingList
                 ShoppingList.remove({});
+
+                // create users
+                const u1 = Accounts.createUser({username: user1_name});
+                const u2 = Accounts.createUser({username: user2_name});
+                const u3 = Accounts.createUser({username: user3_name});
+                // create group and add members
+                const usersList = [ u1, u2, u3 ];
+
+                const groupId = GroupList.insert({
+                    name: groupName,
+                    users: usersList
+                });
+                // add group to user
+                for(const userId of usersList) {
+                    Meteor.users.update({
+                        _id: userId
+                    }, {
+                        $set: {
+                            'services.groups': [ groupId ]
+                        }
+                    });
+                }
+
             });
 
             it('can add new shopping', () => {
@@ -50,8 +75,34 @@ if( Meteor.isServer ) {
             });
 
             it('can add new group shopping', () => {
-                // TODO
+                const newShopping = Meteor.server.method_handlers['checkout.create'];
+
+                const buyer = Meteor.users.findOne({username: user3_name});
+                const indebted = 'group/'+groupName;
+                const price = 123.45;
+                const products = 'Products...'
+
+                const shopping = {
+                    buyer: buyer.username,
+                    indebted: indebted,
+                    products: products,
+                    price: price,
+                };
+
+                newShopping.apply(this, [ shopping ]);
+
+                // assertions...
+                assert.equal(ShoppingList.find({}).count(), 2);
+
+                const result = ShoppingList.findOne({indebtedName: user2_name});
+
+                assert.equal(result.buyer, buyer._id);
+                assert.equal(result.buyerName, buyer.username);
+                assert.equal(result.indebted, Meteor.users.findOne({username: user2_name})._id);
+                assert.equal(result.indebtedName, user2_name);
+                assert.equal(result.price, price);
+                assert.equal(result.products, products);
             });
-        })
+        });
     });
 }
