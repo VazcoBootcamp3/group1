93fa59cf3bf61092c440fab0413aa94dedda1e4b
_michal <sz-m@users.noreply.github.com>
2016-06-30 20:31:56 +0200

    add .toFixed(2)

--- imports/ui/Item/ItemYouShouldPay.jsx
+++ imports/ui/Item/ItemYouShouldPay.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import {Meteor} from 'meteor/meteor';
 
 // material-ui
 import Subheader from 'material-ui/Subheader';
 
-const ItemYouShouldPay = (props) =>
-	<div className="item-box">
-		<Subheader>You should pay:</Subheader>
-		<p className="item-t item-cost item-your-cost">{props.cost}</p>
-	</div>;
+const ItemYouShouldPay = (props) => {
+	return(
+		<div className="item-box">
+			<Subheader>You should pay:</Subheader>
+			<p className="item-t item-cost item-your-cost">{props.cost.toFixed(2)} zł</p>
+		</div>
+	);
+};
 
 export default ItemYouShouldPay;
