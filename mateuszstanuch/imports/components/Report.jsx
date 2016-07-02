af2f7df1e1c8c6a1be87d2dbe4f4b83d003c3a2d
Mateusz Stanuch <mateusz.stanuch@gmail.com>
2016-07-02 21:52:37 +0200

    Update: Move components to separate files

--- mateuszstanuch/imports/components/Report.jsx
+++ mateuszstanuch/imports/components/Report.jsx
@@ -1,22 +1,5 @@
 import React from 'react';
-
-const ReportItem = (props) => {
-    return (
-            <div className="col s12 m6">
-                <div className="card blue-grey darken-1">
-                    <div className="card-content white-text">
-                        <span className="card-title">{props.info.name}</span>
-                        <p>
-                            {props.info.balance}
-                        </p>
-                    </div>
-                    <div className="card-action">
-                        <a href="#!" onClick={() => { props.functions.settleDebt(props.info.id) }}>Ureguluj</a>
-                    </div>
-                </div>
-            </div>
-    );
-};
+import { ReportItem } from './ReportItem';
 
 const Report = (props) => {
     if(props.shoppings.length === 0) {
