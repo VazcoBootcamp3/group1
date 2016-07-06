9b899c7a8ecbefaa36632859676064abc822c8b1
_michal <sz-m@users.noreply.github.com>
2016-07-06 21:06:23 +0200

    collections/groups

--- imports/api/Register/RegisterPublications.jsx
+++ imports/api/Register/RegisterPublications.jsx
@@ -1,5 +1,5 @@
 import {Meteor} from 'meteor/meteor';
-import {Groups} from '/imports/api/groups.js';
+import {Groups} from '/imports/api/Collections/groups.js';
 
 Meteor.publish('groups-list', () => {
 	return Groups.find({});
